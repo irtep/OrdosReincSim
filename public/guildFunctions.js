0922fe67fd1550af6f9ec313d4a0008683955901
Glitch (reincsimulator) <none>
2019-07-07 10:44:53 +0000

    update 7jul19 afternoon

--- public/guildFunctions.js
+++ public/guildFunctions.js
@@ -1,27 +1,21 @@
-// jos vaihtuu valitut guild levelit
-/*
-"Guild12_Selection"
-"Guild12_lvls_Selection"
-
-activities.addEventListener("change", function() {
-*/
 
 // tällä voi laskea exp..
 function calcSpentExpGuilds() {
   
 }
 
+// jos vaihtuu guild levelien määrä ni tää aktivoituu.. callattaa update_levels() raceBackgroundGuild.js kohdasta
 function changeInGuildLvls(leveli, kohde) {
   const selectedGuild = document.getElementById('Guild'+kohde+'_Selection').value;
   
   showSkillsAndSpells(selectedGuild, kohde);
-  console.log('cIGL ', selectedGuild, kohde);
 }
 
 // näyttää skillit ja spellit mitä voi treenata
+// kutsutaan racebackgroundguilds.js kohdasta, update_jotain tms kohdasta, heti alusta
 function showSkillsAndSpells(kilta, kohta) {
   // paikka mihin laitetaan tieto:
-  const guildInfo = document.getElementById('guildInfot');
+  const guildInfo = document.getElementById('table1');
   const guildName = document.getElementById('guildName');
   const findRightSelection = 'Guild'+kohta+'_lvls_Selection';
   const selectedLevelsPlace = document.getElementById(findRightSelection);
@@ -30,89 +24,104 @@ function showSkillsAndSpells(kilta, kohta) {
   // haetaan kilta:
   const selectedGuild = allGuilds.filter(guild => kilta === guild.shortName);
   
-  // laitetaan tiedot:
-  guildName.innerHTML = '<tr><td><b><span class= "bold_heading">'+ selectedGuild[0].longName+ '</span></b></td></tr>';
+  //  uusi tablepohjanen: guildInfo taulukon sisaan
+  const skills = [];
+  const spells = [];
+  const skillsAndSpells = selectedGuild[0].mayTrain[0].skillsAndSpells;
   
-    const skills = [];
-    const spells = [];
-    const skillsAndSpells = selectedGuild[0].mayTrain[0].skillsAndSpells;
+  // montako riviä tarvitaan:
+  let rowNumber = null;    
     
-    // etsi skillit ja spellit: 
-    for (let ii = 0; ii < skillsAndSpells.length; ii++) {
+  // etsi skillit ja spellit: 
+  for (let ii = 0; ii < skillsAndSpells.length; ii++) {
       
-      // jos skilli, niin skilleihin, jos ei niin spelleihin
-      skillsAndSpells[ii].skill ? skills.push(skillsAndSpells[ii]) : spells.push(skillsAndSpells[ii]);
+    // jos skilli, niin skilleihin, jos ei niin spelleihin
+    skillsAndSpells[ii].skill ? skills.push(skillsAndSpells[ii]) : spells.push(skillsAndSpells[ii]);
+  }
+  
+  // ensiksi otsikot:
+  guildInfo.innerHTML = '<tr><td><span class= "bold_heading">'+ selectedGuild[0].longName + '</span></td></tr>'+
+  '<tr><td>Skills</td><td></td><td>Exp</td><td>Spells</td><td></td><td>Exp</td>';
+  
+  // katotaas monta riviä tarvitaan:
+  if (skills.length >= spells.length) { rowNumber = skills.length; } else { rowNumber = spells.length; }  
+  
+  let numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
+  let filteredSkillNumbers = null;
+  let filteredSpellNumbers = null;
+  let skillOnTurn = null;
+  let spellOnTurn = null;
+  
+  // sitten kirjotetaan skillit
+  for (let i = 0; i < rowNumber; i++) {
+    
+    // rivi kerrallaan:
+    let skillRow = ''; let spellRow = '';
+    
+    // tee skillirivi: 
+    if (skills.length > i) { 
+      
+      // poistaa spacet nimestä
+      const withoutSpaces = skills[i].name.replace(/\s/g, '');
+      
+      // poista isommat kuin mikä maximi mitä saa treenata
+      filteredSkillNumbers = numbers.filter( numero => numero <= skills[i].levels[selectedLevels-1]);
+      
+      // lisää max prosentti, jos ei ole jo.
+      if (filteredSkillNumbers[filteredSkillNumbers.length-1] !== skills[i].levels[selectedLevels-1]) {filteredSkillNumbers.push(skills[i].levels[selectedLevels-1]);}  
+      
+      // skillin nimi:
+      const nameOfSelectOpt = 'skillPercent'+withoutSpaces;
+      skillOnTurn = nameOfSelectOpt;
+      
+      skillRow = '<tr><td>' + skills[i].name + '</td><td>'+ 
+        // skillin prosenttivalikko:
+        '<select id= "'+nameOfSelectOpt+'" onchange= "calcSpentExpGuilds()"><option>0</option></select></td><td></td>';
     }
     
-    // jos löytyi skillejä, niin kirjoita ne
-    if (skills.length > 0) {
+    // tee spellirivi:  
+    if (spells.length > i) { 
+    
+      // poistaa spacet nimestä
+      const withoutSpacesSpell = spells[i].name.replace(/\s/g, '');
       
-      guildInfo.innerHTML = guildInfo.innerHTML + '<tr><td><b><span class= "first_column first_row">Skills: </b><br></span></td></tr>';
+      // poista isommat kuin mikä maximi mitä saa treenata
+      filteredSpellNumbers = numbers.filter( numero => numero <= spells[i].levels[selectedLevels-1]);
       
-      skills.forEach( (skill) => {
-        const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
-        
-        // poista isommat kuin mikä maximi mitä saa treenata
-        const filteredNumbers = numbers.filter( numero => numero <= skill.levels[selectedLevels-1]);
-        
-        // lisää max prosentti, jos ei ole jo.
-        if (filteredNumbers[filteredNumbers.length-1] !== skill.levels[selectedLevels-1]) {filteredNumbers.push(skill.levels[selectedLevels-1]);}  
-        
-        guildInfo.innerHTML = guildInfo.innerHTML + '<tr><td><form class= "skills first_column" name= "rGs'+ skill.name + '" id= "rGs'+ skill.name + '">'+ skill.name+ ' '+
-          '<select class="select_number_box" id= "skillPercent'+skill.name+'" onchange= "calcSpentExpGuilds()">'+ 
-            '<option>0</option>'+
-          '</select>'+
-          '</form></td></tr>';
-        
-        // lisää numerovaihtoehdot:
-        filteredNumbers.forEach( (numbero) => {
-          const nameOfSelectOpt = 'skillPercent'+skill.name;
-          const o = document.createElement('option');
-          
-          o.text = numbero;
-          o.value = numbero;
-          document.getElementById(nameOfSelectOpt).appendChild(o);
-        });
-      }); 
+      // lisää max prosentti, jos ei ole jo.
+      if (filteredSpellNumbers[filteredSpellNumbers.length-1] !== spells[i].levels[selectedLevels-1]) {filteredSpellNumbers.push(spells[i].levels[selectedLevels-1]);}  
+      
+      // spellin nimi:
+      const nameOfSelectOpt = 'spellPercent'+withoutSpacesSpell;
+      spellOnTurn = nameOfSelectOpt;
+      
+      spellRow = '<td>' + spells[i].name + '</td><td>'+ 
+        // spellin prosenttivalikko:
+        '<select id= "'+nameOfSelectOpt+'" onchange= "calcSpentExpGuilds()"><option>0</option></select></td><td></td></tr>';
     } else { 
       
-      guildInfo.innerHTML = guildInfo.innerHTML + '<tr class= "first_column"><td>No skills available from guild. </td></tr>';
+      spellRow = '</tr>'
     }
     
-    // ja sama spelleille:
-    // jos löytyi skillejä, niin kirjoita ne
-    if (spells.length > 0) {
-      
-      guildInfo.innerHTML = guildInfo.innerHTML + '<tr class= "fourth_column"><td><b>Spells: </b></tr></td>';
-      
-      spells.forEach( (skill) => {
-        const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
-        
-        // poista isommat kuin mikä maximi mitä saa treenata
-        const filteredNumbers = numbers.filter( numero => numero <= skill.levels[selectedLevels-1]);
-        
-        // lisää max prosentti, jos ei ole jo.
-        if (filteredNumbers[filteredNumbers.length-1] !== skill.levels[selectedLevels-1]) {filteredNumbers.push(skill.levels[selectedLevels-1]);}  
-        
-        guildInfo.innerHTML = guildInfo.innerHTML + '<tr><td><form class= "skills fourth_column" name= "rGs'+ skill.name + '" id= "rGs'+ skill.name + '">'+ skill.name+ ' '+
-          '<select class="select_number_box" id= "skillPercent'+skill.name+'" onchange= "calcSpentExpGuilds()">'+ 
-            '<option>0</option>'+
-          '</select>'+
-          '</form></td></tr>';
-        
-        // lisää numerovaihtoehdot:
-        filteredNumbers.forEach( (numbero) => {
-          const nameOfSelectOpt = 'skillPercent'+skill.name;
-          const o = document.createElement('option');
+    // lisätään rivi:
+    guildInfo.innerHTML = guildInfo.innerHTML + skillRow + spellRow;
+    
+    // numero optionit skilleihin:
+        filteredSkillNumbers.forEach( (numbero) => {
+        const o = document.createElement('option');
           
-          o.text = numbero;
-          o.value = numbero;
-          document.getElementById(nameOfSelectOpt).appendChild(o);
-        });
-      }); 
-    } else { 
-      
-      guildInfo.innerHTML = guildInfo.innerHTML + '<span class= "fourth_column>"No spells available from guild. </span><br>';
-    }  
+        o.text = numbero;
+        o.value = numbero;
+        document.getElementById(skillOnTurn).appendChild(o);
+      });
+    // ja spelleihin
+        filteredSkillNumbers.forEach( (numbero) => {
+        const o = document.createElement('option');
+          
+        o.text = numbero;
+        o.value = numbero;
+        document.getElementById(spellOnTurn).appendChild(o);
+      });
+  }
 }
-
+  
